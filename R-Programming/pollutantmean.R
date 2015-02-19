d493cb0543f6abfe0d803de2bb55fc00ee31e80c
Ana Baraldi <avbaraldi@gmail.com>
2015-02-19 14:58:04 -0200

    first question of assignment one: done

--- R-Programming/pollutantmean.R
+++ R-Programming/pollutantmean.R
@@ -5,22 +5,12 @@ source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
 setwd("~/workspace/data-science/R-Programming")
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-    ## 'directory' is a character vector of length 1 indicating
-    ## the location of the CSV files
-     # Get the files names
-    directory <- list.files("specdata")
-    read.csv(directory[1])
-    
-    # First apply read.csv, then rbind
-    myfiles = do.call("rbind", lapply(files, function(x) read.csv(x, stringsAsFactors = FALSE)))
-    
-    ## 'pollutant' is a character vector of length 1 indicating
-    ## the name of the pollutant for which we will calculate the
-    ## mean; either "sulfate" or "nitrate".
-    
-    ## 'id' is an integer vector indicating the monitor ID numbers
-    ## to be used
-    
-    ## Return the mean of the pollutant across all monitors list
-    ## in the 'id' vector (ignoring NA values)
+    directoryFiles <- list.files(directory, full.names=TRUE)
+    dat <- data.frame()
+    for(i in id) {
+        dat <- rbind(dat, read.csv(directoryFiles[i]))
+    } 
+    mean(dat[, pollutant], na.rm = TRUE)
 }
+
+
