2989df279d25a3e0fdc7ef34b29fe83dda1b3f51
Ana Baraldi <avbaraldi@gmail.com>
2015-02-20 15:26:05 -0200

    adding the explanations for each part

--- R-Programming/corr.R
+++ R-Programming/corr.R
@@ -1,5 +1,19 @@
-## Source to submit the parts of the assignment
-source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
+# Part 3
+# Write a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0. A prototype of this function follows
+# corr <- function(directory, threshold = 0) {
+#     ## 'directory' is a character vector of length 1 indicating
+#     ## the location of the CSV files
+#     
+#     ## 'threshold' is a numeric vector of length 1 indicating the
+#     ## number of completely observed observations (on all
+#     ## variables) required to compute the correlation between
+#     ## nitrate and sulfate; the default is 0
+#     
+#     ## Return a numeric vector of correlations
+# }
+# 
+# ## Source to submit the parts of the assignment
+# source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
 
 # Setting the working directory
 setwd("~/workspace/data-science/R-Programming")
--- R-Programming/pollutantmean.R
+++ R-Programming/pollutantmean.R
@@ -1,5 +1,22 @@
+# Part 1
+# Write a function named 'pollutantmean' that calculates the mean of a pollutant (sulfate or nitrate) across a specified list of monitors. The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'. Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data from the directory specified in the 'directory' argument and returns the mean of the pollutant across all of the monitors, ignoring any missing values coded as NA. A prototype of the function is as follows
+# pollutantmean <- function(directory, pollutant, id = 1:332) {
+#         ## 'directory' is a character vector of length 1 indicating
+#         ## the location of the CSV files
+# 
+#         ## 'pollutant' is a character vector of length 1 indicating
+#         ## the name of the pollutant for which we will calculate the
+#         ## mean; either "sulfate" or "nitrate".
+# 
+#         ## 'id' is an integer vector indicating the monitor ID numbers
+#         ## to be used
+# 
+#         ## Return the mean of the pollutant across all monitors list
+#         ## in the 'id' vector (ignoring NA values)
+# }
+
 ## Source to submit the parts of the assignment
-source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
+# source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
 
 # Setting the working directory
 setwd("~/workspace/data-science/R-Programming")
