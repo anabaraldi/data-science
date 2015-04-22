8b24dc65127e1a74770c8fa9054ba2c17635cd1b
Ana Baraldi <avbaraldi@gmail.com>
2015-04-22 18:28:27 -0300

    finished plot1

---
+++ Exploratory-Data/course-project/plot1.R
@@ -0,0 +1,23 @@
+#libraries
+library(dplyr)
+
+# Reading and cleaning data
+unzip("exdata-data-NEI_data.zip")
+
+# This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+groupedNEI <- group_by(NEI, year) %>%
+    summarise(emissionsSum=sum(Emissions)) %>%
+    mutate(emissionsSum=emissionsSum / 10 ^ 3)
+
+png("plot1.png")
+barplot(groupedNEI$emissionsSum, 
+        names.arg=groupedNEI$year,
+        xlab="Year",
+        ylab="Total PM2.5 Emission (in kiloton)",
+        main="Total PM2.5 Emission - 1999 to 2008")
+dev.off()
+
+
